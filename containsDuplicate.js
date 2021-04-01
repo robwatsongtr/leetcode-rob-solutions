706e5b9cd5214249cdbd2affcc7b571cf958ddec
robwatsongtr <rwatso@gmail.com>
2021-03-31 20:52:45 -0700

    formatting

--- containsDuplicate.js
+++ containsDuplicate.js
@@ -3,24 +3,21 @@
  * Given an integer array nums, return true if any value appears at least twice 
  * in the array, and return false if every element is distinct.
  * 
- * 
- * 
  * @param {number[]} nums
  * @return {boolean}
  */
-
 
  var containsDuplicate = function(nums) {
   let counter = {}
   
   // for of is used to iterate over arrays typically
   for(let val of nums) {
-    counter[val] = (counter[val] || 0) + 1;   
+    counter[val] = ( counter[val] || 0 ) + 1;   
   }
   
   // for in is used to iterate over objects  
-  for(let key in counter) {
-      if( counter[key] > 1) return true;
+  for( let key in counter ) {
+    if( counter[key] > 1 ) return true;
   }
   return false; 
 };
