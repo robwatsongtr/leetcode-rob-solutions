30d4094f11cb989ccbe745b799b61950bb724d27
robwatsongtr <rwatso@gmail.com>
2021-07-21 22:42:51 -0700

    single Number rob diamond solution added

--- singleNumber.js
+++ singleNumber.js
@@ -1,4 +1,5 @@
 /*
+Leetcode 136: Single Number
 
 Given a non-empty array of integers nums, every element appears twice 
 except for one. Find that single one.
@@ -7,27 +8,43 @@ You must implement a solution with a linear runtime complexity a
 and use only constant extra space.
 
 My approach: frequency counter / hash map 
-
 */
-
-
 
 /**
  * @param {number[]} nums
  * @return {number}
 */
 
+// opps not O(1) extra space
 
 var singleNumber = function(nums) {
-    
-  // step  1, turn array into hash map (JS object in this case) 
-  // will use 'for of' to iterate over the array 
   let hashmap = {}; 
   for( let val of nums ) {
     hashmap[val] = (hashmap[val] || 0) + 1; 
   }
-  console.log(hashmap);
-
-
+  for( let key in hashmap ) {
+    if( hashmap[key] < 2 ) {
+      return key; 
+    }
+  }
+  return undefined;
 };
 
+console.log( singleNumber([4,1,2,1,2]) ) ; // 4 
+
+// lets do a bit better on space, thanks to the great
+// Robert Diamond. Dropping set manipulation. 
+
+var singleNumberConst = function(nums) {  
+    let seen = new Set([]);
+    for( let val of nums ) {
+      if (seen.has(val)) {
+          seen.delete(val);
+      } else {
+          seen.add(val);
+      }
+    }
+    return seen.entries().next().value[0]
+}
+
+console.log( singleNumberConst([4,1,2,1,2]) ) ; // 4 
