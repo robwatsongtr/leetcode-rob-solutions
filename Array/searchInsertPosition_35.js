a026b6aecd8b519b28af673614a813a39df82a42
robwatsongtr <rwatso@gmail.com>
2021-09-19 15:00:10 -0700

    setting up directories

---
+++ Array/searchInsertPosition_35.js
@@ -0,0 +1,21 @@
+// Search insert position LC 35 easy 
+
+/*
+
+Given a sorted array of distinct integers and a target value, return the 
+index if the target is found. If not, return the index where it would be 
+if it were inserted in order.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+*/
+
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+ var searchInsert = function(nums, target) {
+    
+};
