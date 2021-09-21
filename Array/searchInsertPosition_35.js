2111f1ebdefca0f7ccdc88805c2aba39b06af159
robwatsongtr <rwatso@gmail.com>
2021-09-20 18:10:11 -0700

    leetcode 35 searchInsertPosition

--- Array/searchInsertPosition_35.js
+++ Array/searchInsertPosition_35.js
@@ -11,11 +11,34 @@ You must write an algorithm with O(log n) runtime complexity.
 */
 
 
-/**
- * @param {number[]} nums
- * @param {number} target
- * @return {number}
- */
- var searchInsert = function(nums, target) {
-    
-};
+
+var searchInsert = function(nums, target) {
+  
+  // set up pointers that will be doing the dividing and conquering
+  let beginningPtr = 0; 
+  let endingPtr = nums.length - 1; 
+  let middlePtr
+
+  // do a standard binary search. The loop will
+  // return the correct index if found
+  while( beginningPtr <= endingPtr ) {
+
+    middlePtr = Math.floor( (beginningPtr + endingPtr) / 2); // recalculate middle;
+
+    if( nums[middlePtr] === target ) return middlePtr;
+
+    if( nums[middlePtr] > target ) {
+      endingPtr = middlePtr - 1;
+    } else {
+      beginningPtr = middlePtr + 1; 
+    }
+
+  }
+
+  // otherwise, return the beginning(left) pointer, which will
+  // always be the index where the number would go.  
+  return beginningPtr; 
+
+}
+
+console.log( searchInsert( [1], 0 ) ); 
