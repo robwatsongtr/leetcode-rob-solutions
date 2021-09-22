d37d6fe443354a8a69c07aedc6ad33fba6183498
robwatsongtr <rwatso@gmail.com>
2021-09-22 15:36:37 -0700

    begin move zeros lc 283

---
+++ Array/moveZeros_283.js
@@ -0,0 +1,27 @@
+// Move Zeros. LC 283
+
+/*
+
+Given an integer array nums, move all 0's to the end of it while maintaining t
+he relative order of the non-zero elements.
+
+Note that you must do this in-place without making a copy of the array.
+
+From the hint:
+
+"A two-pointer approach could be helpful here. 
+The idea would be to have one pointer for iterating the array and 
+another pointer that just works on the non-zero elements of the array.""
+
+*/
+
+/**
+* @param {number[]} nums
+* @return {void} Do not return anything, modify nums in-place instead.
+*/
+
+var moveZeroes = function(nums) {
+    
+
+
+};
