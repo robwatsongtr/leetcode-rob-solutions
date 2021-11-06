1533da260aa709201cbf85a151c2f97fd07011eb
robwatsongtr <rwatso@gmail.com>
2021-11-05 18:22:35 -0700

    rename permutation in string LC 567

---
+++ String/permutationInString_567.js
@@ -0,0 +1,21 @@
+/*
+
+Given two strings s1 and s2, return true if s2 contains a permutation of s1, 
+or false otherwise.
+
+In other words, return true if one of s1's permutations is the substring of s2
+
+
+*/
+
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @return {boolean}
+ */
+var checkInclusion = function(s1, s2) {
+
+  
+
+    
+};
