174e963c3fe00c14f46376ed60d8a0c2255e3d62
robwatsongtr <rwatso@gmail.com>
2021-11-27 22:30:47 -0700

    working on LC 1 two sum

--- Array/inProgress/twoSum.js
+++ Array/inProgress/twoSum.js
@@ -9,22 +9,15 @@
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
- */
- var twoSum = function(arr, target) {
-//   const map = {}
-//   const solution = []
-//   arr.forEach((obj, idx) => {
-//       map[obj] = idx
-//   })
-//   for (var i=0; i < arr.length; i++) {
-//       var find = target - arr[i];
-//       if (map[find] && map[find] != i) {
-//           solution.push([i,map[find]])
-//           map[find] = null
-//           map[arr[i]] = null
-//       }
-//   }
-//   return solution
+*/
+
+var twoSum = function(arr, target) {
+
+  // iterate over array and build a hash table:
+  // KEY is the complement!
+  // VALUE is the index!
+  // each iteration, you check back against what has already been placed in the hash map
+  
   
 };
 
