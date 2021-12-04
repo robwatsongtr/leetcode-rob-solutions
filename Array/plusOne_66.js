471e000bdfc2e74af9aa1b11344124d14f1d38a8
robwatsongtr <rwatso@gmail.com>
2021-12-03 17:29:54 -0800

    algo for plus one lc 66

--- Array/plusOne_66.js
+++ Array/plusOne_66.js
@@ -9,18 +9,25 @@ The digits are ordered from most significant to least significant in left-to-rig
 Increment the large integer by one and return the resulting array of digits.
 
 
-*/
+3 cases to consider. Probably can implement in an if - else if - else 
+
+simplest: last digit of the array is not a 9. Just incremet the digit
+
+if last digit of array is a 9, then you increment the digit to the left of the 9 and 
+and you change the 9 to a zero.
+
+finally, if the [0] digit is 9 make a new array that starts with a 1 and has an
+extra zero in it at the end. 
 
 
-/**
- * @param {number[]} digits
- * @return {number[]}
+
 */
+
 
 
 var plusOne = function(digits) {
 
 
-  
+
     
 };
