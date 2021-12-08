fb29865418167c2d35b4587647ca93942f8b7c92
robwatsongtr <rwatso@gmail.com>
2021-12-07 20:32:03 -0800

    deleted old move zeros, finished LC 66 plus one

--- Array/plusOne_66.js
+++ Array/plusOne_66.js
@@ -8,26 +8,49 @@ The digits are ordered from most significant to least significant in left-to-rig
 
 Increment the large integer by one and return the resulting array of digits.
 
+----Pseudocode: --
 
-3 cases to consider. Probably can implement in an if - else if - else 
+3 cases to consider. 
 
 simplest: last digit of the array is not a 9. Just incremet the digit
 
-if last digit of array is a 9, then you increment the digit to the left of the 9 and 
-and you change the 9 to a zero.
+if last digit of array is a 9, then you change the 9 to a zero and the loop
+iterates to the next tens place and increments that. 
 
 finally, if the [0] digit is 9 make a new array that starts with a 1 and has an
 extra zero in it at the end. 
 
-
-
 */
-
 
 
 var plusOne = function(digits) {
 
+  for( let i = (digits.length - 1); i >= 0; i-- ) {
 
-
+    // if less than 9, just increment the current tens place 
+    if( digits[i] < 9 ) {
+      digits[i]++;  
+      return digits; 
+    } 
     
+    // if 9, change current tens place to 0, loop will then increment
+    // the next tens place above since its iterated on to the next tens place.  
+    digits[i] = 0;   
+    
+  }
+
+  // case for 99 or 999 or 9999...etc 
+  let length = digits.length + 1;
+  let newNumber = new Array(length).fill(0) ;
+  newNumber[0] = 1;
+  return newNumber
+ 
 };
+
+
+console.log( plusOne( [4,3,2,1] ) );
+console.log( plusOne( [4,3,2,8] ) ); 
+console.log( plusOne( [4,3,3,9] ) ); 
+console.log( plusOne( [9,9,9,9] ) ); 
+// console.log( plusOne( [4,3,2,8] ) ); 
+
