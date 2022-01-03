9eef7bd3574e662cea019c2d5edf45294bd18b1d
robwatsongtr <rwatso@gmail.com>
2022-01-02 19:53:30 -0800

    begin LC 28 'Implement strStr'

---
+++ String/implementStrStr_28.js
@@ -0,0 +1,23 @@
+/*
+
+Leetcode 28, implement strStr()
+
+Implement strStr().
+
+Return the index of the first occurrence of needle in haystack, 
+or -1 if needle is not part of haystack.
+
+
+*/
+
+
+const strStr = (haystack, needle)  => {
+
+  if( haystack.length === 0 || needle.length === 0 ) return 0;
+
+  
+
+
+
+    
+};
