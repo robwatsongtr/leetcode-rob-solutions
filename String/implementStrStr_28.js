c0d167efb57404bfacd5204c6aec2776a9093ee9
Rob Watson <rwatso@gmail.com>
2022-01-10 23:28:23 -0800

    fixed first conditional

--- String/implementStrStr_28.js
+++ String/implementStrStr_28.js
@@ -19,7 +19,7 @@ the haystack.
 
 const strStr = (haystack, needle) => {
 
-  if( haystack.length === 0 || needle.length === 0 ) return 0;
+  if( !needle ) return 0;
   if( needle.length > haystack.length ) return -1; 
   
   for( let i = 0; i < haystack.length; i++) { 
