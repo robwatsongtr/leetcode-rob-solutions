75a6afad06ce9f4dcd8d03dcaf38210ac84f4349
Rob Watson <rwatso@gmail.com>
2022-01-10 23:27:22 -0800

    finished lc 28, O(n^2)

--- String/implementStrStr_28.js
+++ String/implementStrStr_28.js
@@ -19,36 +19,23 @@ the haystack.
 
 const strStr = (haystack, needle) => {
 
-  // edge cases, either or both strings empty, or needle longer than haystack
   if( haystack.length === 0 || needle.length === 0 ) return 0;
   if( needle.length > haystack.length ) return -1; 
-
-  // lets make some hash maps 
-  let haystackMap = {};
-  let needleMap = {};
   
-  for ( let letter of haystack ) {
-    haystackMap[letter] = (haystackMap[letter] || 0) + 1; 
+  for( let i = 0; i < haystack.length; i++) { 
+    for( let j = 0; j < needle.length; j++) {
+      // console.log( 'string', haystack[i], i,  'pattern', needle[j], j );
+      if( needle[j] !== haystack[i+j] ) {
+        // console.log('break');
+        break ;
+      }
+      if( j === needle.length - 1) {
+        return i; 
+      }
+    }
   }
 
-  for( let letter of needle ) {
-    needleMap[letter] = (needleMap[letter] || 0) + 1; 
-  }
-
-  console.log('haystack map', haystackMap);
-  console.log('needle map ', needleMap);
-
-  // lets do some window sliding 
-  let i = 0;
-  let j = 1; 
-
-  while( j < haystack.length ) {
-
-
-
-  }
-
-  return -1;
+  return -1;   
   
 
 };
