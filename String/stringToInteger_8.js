931fe00001af7b3b8b77c5ed1ea94872469b42f9
robwatsongtr <rwatso@gmail.com>
2022-01-11 23:46:27 -0800

    begin LC 8 string to integer

---
+++ String/stringToInteger_8.js
@@ -0,0 +1,16 @@
+/*
+
+Lc 9 String to Integer
+
+Implement the myAtoi(string s) function, which converts a string 
+to a 32-bit signed integer (similar to C/C++'s atoi function).
+
+
+*/
+
+const myAtoi = (s) => {
+
+  
+
+
+}
