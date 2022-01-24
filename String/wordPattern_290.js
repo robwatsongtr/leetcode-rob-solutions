d78c2654edc3027fbacf067e077062a041ce5005
robwatsongtr <rwatso@gmail.com>
2022-01-23 20:24:54 -0800

    finished LC 290

--- String/wordPattern_290.js
+++ String/wordPattern_290.js
@@ -1,5 +1,4 @@
 /*
-
 LC 290 Word Pattern
 
 Given a pattern and a string s, find if s follows the same pattern.
@@ -7,11 +6,18 @@ Given a pattern and a string s, find if s follows the same pattern.
 Here follow means a full match, such that there is a bijection 
 between a letter in pattern and a non-empty word in s.
 
-create a hashmap to map each character in the pattern to an individual word
-
-
-
 */
+
+// for some reason JS doesn't have a method to find the existence of a value
+// in a map, or at least I can't find one, so here's the work-around
+const valueInMap = (map, value) =>{
+  const values = [...map.values()];
+  if( values.includes(value) ) {
+    return true;
+  }
+  return false; 
+}
+
 
 const wordPattern = (pattern, s) => {
 
@@ -20,42 +26,38 @@ const wordPattern = (pattern, s) => {
 
   if( words.length !== pattern.length  ) return false; 
 
-  console.log(words)
-  console.log(pattern)
-  console.log(words.length);
-
   for ( let i = 0; i < words.length; i++ ) {
     
     let c = pattern.charAt(i);
     
     if( hashmap.has(c) ) {
 
-      if( !(hashmap.get(c) === words(i)) ) {
+      // if the key is already present and corresponding value is not the same 
+      // as the current word, return false 
+      if( !(hashmap.get(c) === words[i]) ) {
         return false; 
       }
 
     } else {
 
-      // if( hashmap )
-
+      // if the key is not present already but the value is present, it means
+      // that value is associated with some other key, so false 
+      if( valueInMap(hashmap, words[i]) ) {
+        return false; 
+      }
+  
     }
 
-
-
     hashmap.set(c, words[i]);
-    
+    console.log(hashmap);
   }
   
-  
-
-  console.log( hashmap );
-  
+  return true
+   
 }
 
-console.log( wordPattern('abcd', 'dog cat cat dog') )
+console.log( wordPattern('aaaa', 'dog cat cat dog') )
 
 
 
 
-// // create a hashmap to map each character in the pattern to an individual word
-  
