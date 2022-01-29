82d37b6aea906aec5abc6835db482636d1fb318f
robwatsongtr <rwatso@gmail.com>
2022-01-28 16:50:14 -0800

    LC 2032 typo in comments

--- Array/twoOutOfThree_2032.js
+++ Array/twoOutOfThree_2032.js
@@ -3,8 +3,8 @@
 /*
 
 Given three integer arrays nums1, nums2, and nums3, return a 
-distinct array containing all the values that are present in at l
-east two out of the three arrays. You may return the values in any order.
+distinct array containing all the values that are present in at 
+least two out of the three arrays. You may return the values in any order.
 
 So values need to be present in arrays: 1 & 2, 1 & 3, and 2 & 3
 
@@ -24,36 +24,27 @@ const twoOutOfThree = (nums1, nums2, nums3) => {
     // console.log(nums1map)
   }
   for( let val of nums2 ) {
-    // console.log(val)
     nums2map[val] = (nums2map[val] || 0) + 1 ;
-    // console.log(nums1map)
   }
   for( let val of nums3 ) {
-    // console.log(val)
     nums3map[val] = (nums3map[val] || 0) + 1 ;
-    // console.log(nums1map)
   }
 
-  //
   // So values need to be present in arrays: 1 & 2, or 1 & 3, or 2 & 3
-  //
-
-  // we got the maps. Now check 1 & 2, and 1 & 3
+  // We got the maps. Now check 1 & 2, and 1 & 3
   for( let key in nums1map) {
     if( key in nums2map || key in nums3map) {
       result.push(key)
     }
   }
 
-  // now check the second array against the third 
+  // now check the second array against the third, don't add a number already there 
   for( let key in nums2map) {
     if( (key in nums3map ) && (!result.includes(key) ) ) {
       result.push(key)
     }
   }
 
-
-  
   return result; 
 
 }
@@ -62,5 +53,5 @@ const twoOutOfThree = (nums1, nums2, nums3) => {
 console.log( twoOutOfThree( [1,2,2], [4,3,3], [5] ) ) 
 
 
-// 
+ 
   
