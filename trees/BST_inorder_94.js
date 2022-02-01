ac3266c219452c9b9daf94fb2c3b06849f1f4831
robwatsongtr <rwatso@gmail.com>
2022-01-31 20:57:21 -0800

    started LC 94 bst inorder traversal

---
+++ trees/BST_inorder_94.js
@@ -0,0 +1,36 @@
+// Leetcode 94 Binary Tree Inorder Traversal
+//
+// Given the root of a binary tree, return the inorder 
+// traversal of its nodes' values.
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+*/
+
+function TreeNode(val, left, right) {
+  this.val = (val===undefined ? 0 : val)
+  this.left = (left===undefined ? null : left)
+  this.right = (right===undefined ? null : right)
+}
+
+
+var inorderTraversal = function(root) {
+
+
+    
+};
+
+
+binarySearchTree = new TreeNode( 1, null, 2 );
+binarySearchTree = new TreeNode();
+binarySearchTree = new TreeNode(1);
