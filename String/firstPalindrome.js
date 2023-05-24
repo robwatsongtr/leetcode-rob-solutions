0dc7ac8bdf69b0c027f2d92ba3a0511445133ad1
robwatsongtr <rwatso@gmail.com>
2023-05-24 11:33:41 -0700

    finished LC 2018

--- String/firstPalindrome.js
+++ String/firstPalindrome.js
@@ -5,7 +5,31 @@ Given an array of strings words, return the first palindromic string in the arra
 If there is no such string, return an empty string "".
 
 A string is palindromic if it reads the same forward and backward.
-
-
-
 */
+
+const firstPalindrome = function(words) {
+
+  function isPalindrome(str) {
+    let lptr = 0
+    let rptr = str.length - 1
+    while (lptr <= rptr) {
+      if( str[lptr] !== str[rptr]) {
+        return false
+      }
+      lptr++
+      rptr--
+    }
+    return true
+  }
+
+  for (str of words) {
+    let checkIfPalindrome = isPalindrome(str)
+    if(checkIfPalindrome) return str
+  }  
+    
+  return ''
+};
+
+
+
+console.log( firstPalindrome(["def","ghi"]) )
