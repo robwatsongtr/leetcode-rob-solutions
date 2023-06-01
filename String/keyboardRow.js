8079f4c0cf1128d281244877e0643e8688551bab
robwatsongtr <rwatso@gmail.com>
2023-06-01 08:33:39 -0700

    LC 500 keyboard Row

--- String/keyboardRow.js
+++ String/keyboardRow.js
@@ -18,6 +18,31 @@ var findWords = function(words) {
   let result = []
 
   for( let word of words ) {
+    let isInRow1 = true;
+    let isInRow2 = true;
+    let isInRow3 = true;
+    // Need to check if ALL the characters of a word are in the same row.
+    // add a flag for each row and break the loop 
+    // if any character is found in a different row.
+    for (let i = 0; i < word.length; i++) {
+      let char = word[i].toLowerCase();
+      if (!row1.includes(char)) {
+        isInRow1 = false;
+      }
+      if (!row2.includes(char)) {
+        isInRow2 = false;
+      }
+      if (!row3.includes(char)) {
+        isInRow3 = false;
+      }
+      if (!isInRow1 && !isInRow2 && !isInRow3) {
+        break;
+      }
+    }
+
+    if (isInRow1 || isInRow2 || isInRow3) {
+      result.push(word);
+    }
     
   }
 
