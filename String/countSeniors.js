5e93a79fc5d17c4a0494fd238c13dbcc89812a1c
robwatsongtr <rwatso@gmail.com>
2023-06-07 13:39:17 -0700

    finished LC 2678 Number of Senior Citizens

--- String/countSeniors.js
+++ String/countSeniors.js
@@ -22,5 +22,16 @@ Explanation: None of the passengers are older than 60.
 */
 
 var countSeniors = function(details) {
-    
+  let count = 0
+  for( let detail of details) {
+    let age = parseInt(detail[11] + detail[12] )
+    console.log(`age: ${age}`)
+    if (age > 60) count++
+  }
+  return count
 };
+
+
+console.log( countSeniors( ["7868190130M7522","5303914400F9211","9273338290F4010"]))
+
+console.log( countSeniors( ["1313579440F2036","2921522980M5644"]) )
