6a456deb8e03f01618e881761e4b682ef6a6016e
robwatsongtr <rwatso@gmail.com>
2023-06-08 09:57:44 -0700

    LC 1002 find common characters begin

---
+++ String/findCommonChars.js
@@ -0,0 +1,22 @@
+/*
+LC 1002 Find Common Characters
+
+Given a string array words, return an array of all characters that show 
+up in all strings within the words (including duplicates). 
+You may return the answer in any order.
+
+Input: words = ["bella","label","roller"]
+Output: ["e","l","l"]
+
+Input: words = ["cool","lock","cook"]
+Output: ["c","o"]
+
+
+*/
+
+
+var commonChars = function(words) {
+    
+};
+
+
