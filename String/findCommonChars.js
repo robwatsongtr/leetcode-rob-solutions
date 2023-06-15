2a49667421aee147d68d9ca9af7b5a59fc8d3cce
robwatsongtr <rwatso@gmail.com>
2023-06-14 21:06:51 -0700

    working on LC 1002 find common chars

--- String/findCommonChars.js
+++ String/findCommonChars.js
@@ -13,14 +13,41 @@ Output: ["c","o"]
 
 Approach:
 
-Iterate 
+Iterate through each word in the word array:
+  for each word call a function that returns a character frequency hashmap for that word
+
+Compare each hash map against every other hashmap and find the common characters
+
+
 
 
 */
 
 
 var commonChars = function(words) {
-    
+  arrayOfObjs = []
+  function getMap(str) {
+    let map = {}
+    for( let val of str) {
+      map[val] = ( map[val] || 0) + 1
+    }
+    return map 
+  }
+
+  console.log(words)
+
+  for( let word of words )  {
+    let obj = getMap(word)
+    arrayOfObjs.push(obj)
+  }
+
+  console.log(arrayOfObjs)
+ 
+  const commonKeys = Object.keys(arrayOfObjs[0])
+
+  console.log(commonKeys)
+
 };
 
 
+console.log( commonChars(["bella","label","roller"]))
