7fafa7349479f175da6b4c149b7278973e480c20
robwatsongtr <rwatso@gmail.com>
2023-06-26 16:58:25 -0700

    reverse string using arithmetic swap function

---
+++ non-leetcode-questions/arithSwapRev.js
@@ -0,0 +1,47 @@
+/*
+Aritmetically Swap two numbers
+
+Use swap funciton to reverse a string.
+
+*/
+
+
+const arithSwap = (a, b) => {
+  a = b + a
+  b = a - b
+  a = a - b
+  return [a, b]
+}
+
+const revString = (str) => {
+  
+  let strArr = []
+  let result = []
+
+  for( let i = 0; i < str.length; i++) {
+   let ltr = str.charCodeAt(i)
+   strArr.push(ltr)
+  }
+
+  let rightPtr = strArr.length - 1
+  let leftPtr = 0
+  
+  while( leftPtr < rightPtr ) {
+    const[ newLeft, newRight] = arithSwap( strArr[leftPtr], strArr[rightPtr] )
+    strArr[leftPtr] = newLeft
+    strArr[rightPtr] = newRight
+    leftPtr++
+    rightPtr--
+  } 
+
+  for( let i = 0; i < strArr.length; i++ ) {
+    let ltr = String.fromCharCode(strArr[i])
+    result.push(ltr)
+  }
+
+  return result.join('')
+
+}
+
+
+console.log( revString('Mightily') )
