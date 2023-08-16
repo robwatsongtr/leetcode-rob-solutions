120743427d3b443afac83aad3732722961c84cff
robwatsongtr <rwatso@gmail.com>
2023-08-16 12:26:51 -0700

    got a good solution for LC 318 max product

--- String/maxProduct.js
+++ String/maxProduct.js
@@ -1,6 +1,4 @@
-// LC 318 Maximum Product of Word Lengths 
-
-/*
+/* LC 318 Maximum Product of Word Lengths 
 
 Given a string array words, return the maximum value of length(word[i]) * length(word[j]) 
 where the two words do not share common letters. If no such two words exist, return 0.
@@ -23,12 +21,45 @@ Input: words = ["a","aa","aaa","aaaa"]
 Output: 0
 Explanation: No such pair of words.
 
+Pattern: Probably use a hashmap to figure out 
+
 */
+
+
 
 /**
  * @param {string[]} words
  * @return {number}
  */
 var maxProduct = function(words) {
-    
+  // get distinct letters for every word 
+  const letters = words.map( word => [...new Set(word)] )
+
+  let result = 0
+
+  // i begins with first word
+  for( let i = 0; i < words.length - 1; i++ ) {
+
+    // j is always the word after i 
+    for( let j = i + 1; j < words.length; j++) {
+
+      // if any letter in letters[j](words[j]) is found in letters[i](words[i]), 
+      // then two words share common letters. 'Some' tests whether at least one element
+      // in the array passes the test implemented by the provided function 
+      if ( !letters[i].some( item => letters[j].includes(item) ) ) {
+
+        // if they don't share common letters get the length product
+        let prod = words[i].length * words[j].length
+
+        // keep updating the result with the bigger product 
+        if (prod > result) result = prod
+        
+      }
+
+    }
+  } 
+
+  return result  
 };
+
+console.log( maxProduct(["abcw","baz","foo","bar","xtfn","abcdef"]))
