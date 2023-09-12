07126b5103891fb868d2c4ed09fb55d4ab8a2aa6
robwatsongtr <rwatso@gmail.com>
2023-09-12 13:45:54 -0700

    updated removeDuplicates 26

--- Array/removeDuplicates_26.js
+++ Array/removeDuplicates_26.js
@@ -12,20 +12,23 @@
 // uses the "two pointer method" to solve problem 
 
  var removeDuplicates = function(nums) {
-    if (nums.length === 0) return 0;
-    let i = 0;
-    let j = 1;
+  if (nums.length === 0) return 0;
+  let i = 0;
+  let j = 1;
 
-    // i and j start on [0] and [1]
-    // Increment j as long as there is a duplicate.
-    // When dupliate run ends, increment i, copy j to i+1
-    // when j reaches end of the array, return i 
-    for( j ; j < nums.length ; j++) {
-      if (nums[j] != nums[i] ) i++;
+  // i and j start on [0] and [1]
+  // Increment j as long as there is a duplicate.
+  // When dupliate run ends, increment i and copy j to i 
+  // when j reaches end of the array, return i + 1 for length of unique elements 
+  for( j ; j < nums.length ; j++) {
+    if (nums[j] !== nums[i] ){ 
+      i++;
       nums[i] = nums[j]
     }
-    return i + 1;  // returns the length of the array without duplicates
+  }
+  return i + 1; 
 
 };
 
 console.log( removeDuplicates( [0,0,1,1,1,2,4,4,5] ) )
+
