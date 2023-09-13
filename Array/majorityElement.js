5f167ec7e4b842569dfce301875d2611a6e83c70
robwatsongtr <rwatso@gmail.com>
2023-09-13 14:30:34 -0700

    finished 169

---
+++ Array/majorityElement.js
@@ -0,0 +1,45 @@
+/*
+169. Majority Element
+
+Given an array nums of size n, return the majority element.
+
+The majority element is the element that appears more than ⌊n / 2⌋ times. 
+You may assume that the majority element always exists in the array.
+
+Example 1:
+
+Input: nums = [3,2,3]
+Output: 3
+Example 2:
+
+Input: nums = [2,2,1,1,1,2,2]
+Output: 2
+
+Approach:
+
+Hashmap / Frequency counter to get number of occurences of each element
+
+Find the one that has a frequency at or greater than 50 percent of the number 
+of elements in array
+
+*/
+
+
+const majorityElement = (nums) => {
+ 
+  let freqHash = {}
+  let majorityVal = nums.length / 2 
+
+  for( let val of nums) {
+    freqHash[val] = ( freqHash[val] || 0 ) + 1
+  }
+
+  for( let key in freqHash) {
+    if( freqHash[key] >= majorityVal) 
+    return key
+  }
+
+}
+
+
+console.log( majorityElement([3,2,3]))
