a3641a031f81f47d60d3e0e82395b7b8551ffc76
robwatsongtr <rwatso@gmail.com>
2023-10-19 14:49:17 -0700

    addBinary

--- bit_manipulation/addBinary.js
+++ bit_manipulation/addBinary.js
@@ -16,22 +16,43 @@ Output: "10101"
 
 const addBinary = (a, b) => {
 
+  // helper function to pad binary string with zeros 
   function padWithZeros(str, length) {
-    for( let i = str.length; i < length; i++) {
+    for( let i = str.length; i < length; i++ ) {
       str = '0' + str 
     }
-    
     return str 
   }
  
+  // figure out the length of the longer binary string 
   const strMaxLength = Math.max( a.length, b.length )
   
-  bString1 = padWithZeros(a, strMaxLength)
-  bString2 = padWithZeros(b, strMaxLength)
+  // pad with zeros the shorter of the binary strings 
+  const bString1 = padWithZeros(a, strMaxLength)
+  const bString2 = padWithZeros(b, strMaxLength)
 
-  console.log(bString1, bString2)
+  let result = ""
+  let carry = 0
+ 
+  // iterate in reverse from the least significat bit ( rightmost / end )
+  for( let i = strMaxLength - 1; i >= 0; i--) {
+    let bit1 = parseInt(bString1[i], 2) // parse to base 2
+    let bit2 = parseInt(bString2[i], 2) // parse to base 2 
+
+    const sum = bit1 ^ bit2 ^ carry;
+    carry = (bit1 & bit2) | (carry & (bit1 ^ bit2));
+
+    result = sum.toString() + result;
+
+  }
+
+  if (carry) {
+    result = '1' + result;
+  }
+
+  return result 
 
 }
 
 
-console.log( addBinary('0110', '1110110'))
+console.log( addBinary('10110', '00110'))
