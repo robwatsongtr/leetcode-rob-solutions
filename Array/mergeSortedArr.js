90555719ad5be7029f914c0ffb6043f63feaf8f5
robwatsongtr <rwatso@gmail.com>
2023-11-07 15:20:14 -0800

    merge sorted arr

--- Array/mergeSortedArr.js
+++ Array/mergeSortedArr.js
@@ -8,9 +8,11 @@ You are given two integer arrays nums1 and nums2, sorted in non-decreasing order
 Merge nums1 and nums2 into a single array sorted in non-decreasing order.
 
 The final sorted array should not be returned by the function, but instead be stored 
-inside the array nums1. To accommodate this, __nums1 has a length of m + n___, 
-where the first m elements denote the elements that should be merged, and the last n e
-lements are set to 0 and should be ignored. nums2 has a length of n.
+inside the array nums1. To accommodate this, nums1 has a length of m + n, 
+where the first m elements denote the elements that should be merged, and the last n 
+elements are set to 0 and should be ignored. 
+
+nums2 has a length of n.
 
  
 
@@ -52,6 +54,7 @@ const merge = (nums1, m, nums2, n) => {
       p1--;
     } else {
       // otherwise nums2 pointer (p2) is bigger so copy that over to 'i' in nums1
+      // decrement pointer in nums2 (p2)
       nums1[i] = nums2[p2];
       p2--;
     }
