5049830fe814eddc304ae02ead9f97375d7b07a2
robwatsongtr <rwatso@gmail.com>
2023-11-13 18:19:07 -0800

    remove duplicates 26 in python

--- python_solutions/array_string/merge_sorted_88.py
+++ python_solutions/array_string/merge_sorted_88.py
@@ -42,13 +42,6 @@ Note that because m = 0, there are no elements in nums1. The 0 is only there to 
 
 class Solution(object):
     def merge(self, nums1, m, nums2, n):
-        """
-        :type nums1: List[int]
-        :type m: int
-        :type nums2: List[int]
-        :type n: int
-        :rtype: None Do not return anything, modify nums1 in-place instead.
-        """
         p1 = m - 1  # Index for nums1
         p2 = n - 1  # Index for nums2
         i = m + n - 1  # Index for the merged array (nums1)
--- python_solutions/array_string/remove_Duplicates_26.py
+++ python_solutions/array_string/remove_Duplicates_26.py
@@ -1,4 +1,35 @@
 """
+26. Remove Duplicates from Sorted Array
 
+Given an integer array nums sorted in non-decreasing order, remove the duplicates 
+in-place such that each unique element appears only once. 
+The relative order of the elements should be kept the same. 
+Then return the number of unique elements in nums.
+
+Consider the number of unique elements of nums to be k, to get accepted, 
+you need to do the following things:
+
+Change the array nums such that the first k elements of nums contain 
+the unique elements in the order they were present in nums initially.
+The remaining elements of nums are not important as well as the size of nums.
+
+Return k.
 
 """
+
+class Solution(object):
+	def removeDuplicates(self, nums):
+		if len(nums) == 0:
+      		return 0
+
+    	i = 0
+    	j = 1
+
+    	while j < len(nums):
+      		if nums[j] == nums[i]:
+        		j += 1
+			else:
+				i += 1
+				nums[i] = nums[j]
+
+    	return i + 1 
