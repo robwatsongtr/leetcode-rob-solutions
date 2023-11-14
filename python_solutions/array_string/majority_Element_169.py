6df0b76ee1a5c5dd49f62dd8e3a7a17ff725c021
robwatsongtr <rwatso@gmail.com>
2023-11-14 14:07:35 -0800

    majority element 169 python)

---
+++ python_solutions/array_string/majority_Element_169.py
@@ -0,0 +1,31 @@
+"""
+169. Majority Element
+
+Given an array nums of size n, return the majority element.
+
+The majority element is the element that appears more than ⌊n / 2⌋ times.
+ You may assume that the majority element always exists in the array.
+
+Example 1:
+
+Input: nums = [3,2,3]
+Output: 3
+
+Example 2:
+
+Input: nums = [2,2,1,1,1,2,2]
+Output: 2
+
+"""
+
+class Solution(object):
+    def majorityElement(self, nums):
+        my_dict = {}
+        majority_val = len(nums) // 2
+
+        for val in nums:
+            my_dict[val] = my_dict.get(val, 0) + 1
+
+        for key in my_dict:
+            if my_dict[key] > majority_val:
+                return key
