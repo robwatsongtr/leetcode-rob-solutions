6f194a9d96d926bce276fc6c0af28af24ee6f31f
robwatsongtr <rwatso@gmail.com>
2023-11-16 07:55:22 -0800

    finished longest common prefix python

--- python_solutions/array_string/longest_prefix_14.py
+++ python_solutions/array_string/longest_prefix_14.py
@@ -17,4 +17,17 @@ Each iteration shorten the prefix based on what's there in the other strings
 
 class Solution(object):
     def longestCommonPrefix(self, strs):
+        if len(strs) == 0: 
+            return ''
+
+        # make the entire first string as the prefix. clever!
+        prefix = strs[0]
+
+        for s in strs:
+            while not s.startswith(prefix):
+                # slice prefix by one character from the end
+                prefix = prefix[:-1]
+                if not prefix:
+                    return ''
         
+        return prefix
