447b1a9d749ceb682ceddf3bcada37e558513951
robwatsongtr <rwatso@gmail.com>
2023-11-16 07:29:33 -0800

    begin longest common prefix python

---
+++ python_solutions/array_string/longest_prefix_14.py
@@ -0,0 +1,15 @@
+"""
+LC 14
+
+Longest Common Prefix
+
+Write a function to find the longest common prefix string amongst an array of strings.
+If there is no common prefix, return an empty string ""
+
+Approach:
+
+make the entire first string as the prefix.
+Each iteration shorten the prefix based on what's there in the other strings
+
+
+"""
