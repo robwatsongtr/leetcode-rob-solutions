5a6026cc44072141a725c8aba0022ffbee8473ad
robwatsongtr <rwatso@gmail.com>
2023-11-16 07:25:17 -0800

    length of last word 58 python

---
+++ python_solutions/array_string/length_last_word_58.py
@@ -0,0 +1,23 @@
+"""
+58. Length of Last Word
+
+Given a string s consisting of words and spaces, return the length of the 
+last word in the string.
+
+A word is a maximal substring consisting of non-space characters only.
+
+"""
+
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        # remove any and all whitespace and split string into list of words 
+        processed_str = s.strip().split()
+
+        if not processed_str:
+            return 0
+
+        # negative indexing access the last element of the list 
+        last_word = processed_str[-1]
+        length = len(last_word)
+
+        return length 
