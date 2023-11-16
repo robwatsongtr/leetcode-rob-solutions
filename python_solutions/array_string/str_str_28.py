8715d5c0088a531e05febdae052171a97a1fb63b
robwatsongtr <rwatso@gmail.com>
2023-11-16 14:00:25 -0800

    str str python done

--- python_solutions/array_string/str_str_28.py
+++ python_solutions/array_string/str_str_28.py
@@ -13,9 +13,21 @@ the haystack.
 
 """
 
-
-
-
 class Solution(object):
     def strStr(self, haystack, needle):
+        if not needle:
+            return 0
+
+        if len(needle) > len(haystack):
+            return -1 
+
+        for i in range(len(haystack) - len(needle) + 1):
+            j = 0
+            while j < len(needle) and haystack[i + j] == needle[j]:
+                j += 1
+            
+            if j == len(needle):
+                return i 
+
+        return -1
         
