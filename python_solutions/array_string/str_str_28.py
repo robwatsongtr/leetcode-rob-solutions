d23565291410686003563c65810ad514df2e49fb
robwatsongtr <rwatso@gmail.com>
2023-11-16 13:37:33 -0800

    begin str_str in python

---
+++ python_solutions/array_string/str_str_28.py
@@ -0,0 +1,21 @@
+"""
+Leetcode 28, implement strStr()
+
+Implement strStr().
+
+Return the index of the first occurrence of needle in haystack, 
+or -1 if needle is not part of haystack.
+
+-haystack is an input string
+
+-needle is a contiguous substring that is being searched for in 
+the haystack. 
+
+"""
+
+
+
+
+class Solution(object):
+    def strStr(self, haystack, needle):
+        
