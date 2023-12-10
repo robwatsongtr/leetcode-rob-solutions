6890c82de5665bff27732b93f5b4cae580b040af
robwatsongtr <rwatso@gmail.com>
2023-12-09 16:55:44 -0800

    comments on 28 strStr

--- python_solutions/array_string/str_str_28.py
+++ python_solutions/array_string/str_str_28.py
@@ -21,13 +21,19 @@ class Solution(object):
         if len(needle) > len(haystack):
             return -1 
 
+        # iterate one i at a time through haystack:
+        # the range takes into account the length of the needle so you don't
+        # go all the way to the end of the haystack, which would be inefficient
         for i in range(len(haystack) - len(needle) + 1):
             j = 0
+            # inner loop will try and loop the length of needle within the haysta cj
             while j < len(needle) and haystack[i + j] == needle[j]:
                 j += 1
             
+            # if we get to the end of the needle we have a match and return 
+            # the starting index 
             if j == len(needle):
                 return i 
 
-        return -1
+        return -1 # no match
         
