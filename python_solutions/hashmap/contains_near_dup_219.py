0d93edf3cef4149be96a7c7c6e88e5c94f453cdd
robwatsongtr <rwatso@gmail.com>
2023-12-12 14:49:08 -0800

    contains nearby dup 219 in python

---
+++ python_solutions/hashmap/contains_near_dup_219.py
@@ -0,0 +1,37 @@
+"""
+219. Contains Duplicate II
+
+Given an integer array nums and an integer k, return true if there are two distinct 
+indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
+
+Example 1:
+
+Input: nums = [1,2,3,1], k = 3
+Output: true
+
+Example 2:
+
+Input: nums = [1,0,1,1], k = 1
+Output: true
+
+Example 3:
+
+Input: nums = [1,2,3,1,2,3], k = 2
+Output: false
+
+"""
+
+class Solution(object):
+    def containsNearbyDuplicate(self, nums, k):
+        hashmap = {}
+
+        for i, current_num in enumerate(nums):
+            if current_num in hashmap:
+                j = hashmap[current_num]
+                if abs(i - j) <= k:
+                    return True 
+
+            hashmap[current_num] = i
+
+        return False 
+        
