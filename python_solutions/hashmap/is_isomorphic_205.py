19c4881247acd33fd83c88dab821034ab590683e
robwatsongtr <rwatso@gmail.com>
2023-12-14 09:24:35 -0800

    is isomorphic 205 in python

---
+++ python_solutions/hashmap/is_isomorphic_205.py
@@ -0,0 +1,55 @@
+"""
+205. Isomorphic Strings
+
+Given two strings s and t, determine if they are isomorphic.
+
+Two strings s and t are isomorphic if the characters in s can be replaced 
+to get t.
+
+All occurrences of a character must be replaced with another character 
+while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
+
+ 
+Example 1:
+
+Input: s = "egg", t = "add"
+Output: true
+
+Example 2:
+
+Input: s = "foo", t = "bar"
+Output: false
+
+Example 3:
+
+Input: s = "paper", t = "title"
+Output: true
+
+"""
+
+class Solution(object):
+    def isIsomorphic(self, s, t):
+        if len(s) != len(t):
+            return false
+
+        dict_s = {}
+        dict_t = {}
+
+        # if a character in s isn't mapped to a character in t, do so and add it to map
+        # if that characer is in the map and its mapped to a different character, return false
+        for charS, charT in zip(s, t):
+            if charS not in dict_s:
+                dict_s[charS] = charT
+            else:
+                if dict_s[charS] != charT:
+                    return False   
+
+            if charT not in dict_t:
+                dict_t[charT] = charS
+            else:
+                if dict_t[charT] != charS:
+                    return False
+
+        return True
+
+    
