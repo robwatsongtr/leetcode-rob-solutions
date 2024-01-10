bc52cbb72b1e8f124fa0c93c086b665db867296d
robwatsongtr <rwatso@gmail.com>
2024-01-10 09:05:09 -0800

    implement graph again

--- graph.py
+++ graph.py
@@ -34,3 +34,55 @@ class Graph:
                     queue.append(neighbor)
 
         return result 
+    
+    def dfs(self, start_node, visited=None):
+        result = []
+
+        if visited is None:
+            visited = set()
+
+        def traverse(node):
+            if node is None:
+                return 
+            
+            visited.add(node)
+            result.append(node)
+
+            for neighbor in self.adjacency_list.get(node, []):
+                if neighbor not in visited:
+                    traverse(neighbor)
+
+        traverse(start_node)
+
+        return result 
+    
+
+if __name__ == "__main__":
+
+    g = Graph()
+
+    g.add_node("A")
+    g.add_node("B")
+    g.add_node("C")
+    g.add_node("D")
+    g.add_node("E")
+
+    g.add_edge_undirected("A", "B")
+    g.add_edge_undirected("A", "C")
+    g.add_edge_undirected("B", "E")
+    g.add_edge_undirected("B", "D")
+    g.add_edge_undirected("D", "E")
+
+    print(g.adjacency_list)
+    print(f"BFS from E: { g.bfs("E") }")
+    print(f"DFS from E: { g.dfs("E") }")
+
+
+
+    
+
+
+    
+
+    
+
