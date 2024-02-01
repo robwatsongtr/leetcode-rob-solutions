e52f3da2846b4bf8d8ca6e8334b9bebc0236de6e
robwatsongtr <rwatso@gmail.com>
2024-02-01 14:40:20 -0800

    linked list cycle

--- linked_List/linkedListCycle.js
+++ linked_List/linkedListCycle.js
@@ -22,11 +22,7 @@ Input: head = [1,2], pos = 0
 Output: true
 Explanation: There is a cycle in the linked list, where the tail connects to the 0th node.
 
-Example 3:
 
-Input: head = [1], pos = -1
-Output: false
-Explanation: There is no cycle in the linked list
 
 */
 
