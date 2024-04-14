4fd8c54ac64dc7089c7e8eef8e3186b7c73c88bf
robwatsongtr <rwatso@gmail.com>
2024-04-13 17:57:28 -0700

    random generator

--- python_solutions/random_LC_top150.py
+++ python_solutions/random_LC_top150.py
@@ -7,15 +7,21 @@ def choose_random_150():
         100, 226, 101, 112, 222, 637, 530, 108, 35
     ]
 
+    array_list_easy = [
+        88, 27, 26, 80, 169, 121, 13, 58, 14, 28
+    ]
+
     medium_list = [
         80
     ]
  
     random_easy = random.choice(easy_list)
     random_medium = random.choice(medium_list)
+    random_array_list_easy = random.choice(array_list_easy)
 
-    print(f"Random easy question: {random_easy}")
-    print(f"Random medium question: {random_medium}")
+    # print(f"Random easy question: {random_easy}")
+    # print(f"Random medium question: {random_medium}")
+    print(f"Random array easy question: {random_array_list_easy}")
 
 
 if __name__ == "__main__":
