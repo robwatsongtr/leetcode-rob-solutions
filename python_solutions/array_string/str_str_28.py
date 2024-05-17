1c59efa351436b55220bb5e36ea0566e24079985
robwatsongtr <rwatso@gmail.com>
2024-05-17 11:58:41 -0700

    str_str python

--- python_solutions/array_string/str_str_28.py
+++ python_solutions/array_string/str_str_28.py
@@ -35,4 +35,25 @@ class Solution(object):
                 return i 
 
         return -1 # no match
+    
+
+class Solution2(object):
+    def strStr(self, haystack: str, needle: str) -> int:
+        if len(needle) > len(haystack):
+            return -1
+
+        i = 0
+
+        while i < len(haystack) - len(needle) + 1:
+            j = 0
+
+            while j < len(needle) and haystack[i + j] == needle[j]:
+                j += 1
+
+            if j == len(needle):
+                return i
+
+            i += 1
+
+        return -1
         
