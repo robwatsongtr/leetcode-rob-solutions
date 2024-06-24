76082f5e3d5a6272dd00bcacb54b33c871e4f7d1
robwatsongtr <rwatso@gmail.com>
2024-06-24 09:14:57 -0700

    merge sorted array python change variable name

--- python_solutions/array_string/merge_sorted_88.py
+++ python_solutions/array_string/merge_sorted_88.py
@@ -22,23 +22,22 @@ nums2 has a length of n.
 
 class Solution(object):
     def merge(self, nums1, m, nums2, n):
-        n1p = m - 1  # Index for nums1
-        n2p = n - 1  # Index for nums2
+        p1 = m - 1  # Index for nums1
+        p2 = n - 1  # Index for nums2
         n1end = m + n - 1  # Index for end of nums 1 including padding 0's
 
-        while n1p >= 0 and n2p >= 0:
-            if nums2[n2p] > nums1[n1p]:
-                nums1[n1end] = nums2[n2p]
-                n2p -= 1
-
+        while p1 >= 0 and p2 >= 0:
+            if nums2[p2] > nums1[p1]:
+                nums1[n1end] = nums2[p2]
+                p2 -= 1
             else:
-                nums1[n1end] = nums1[n1p]
-                n1p -= 1
+                nums1[n1end] = nums1[p1]
+                p1 -= 1
                 
             n1end -= 1
 
         # If there are remaining elements in nums2 merge the rest to nums1
-        while n2p >= 0:
-            nums1[n1end] = nums2[n2p]
-            n2p -= 1
+        while p2 >= 0:
+            nums1[n1end] = nums2[p2]
+            p2 -= 1
             n1end -= 1
