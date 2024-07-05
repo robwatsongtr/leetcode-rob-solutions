f373fb25fb6f7cce953a12d5c6d66cf2ac39c356
robwatsongtr <rwatso@gmail.com>
2024-07-05 09:36:41 -0700

    is subsequendce

--- python_solutions/two_pointers/is_Subsequence_392.py
+++ python_solutions/two_pointers/is_Subsequence_392.py
@@ -24,8 +24,7 @@ class Solution(object):
 
         # while the long pointer hasnt reached the end of the long string: 
         while tPtr < len(t):
-            # increment short pointer as long as there is a match
-            # in long string / pointer
+            # increment short pointer as long as there is a match in long string / pointer
             if s[sPtr] == t[tPtr]: 
                 sPtr += 1
 
@@ -40,4 +39,27 @@ class Solution(object):
         # didn't get to end of short string so not a subsequence
         return False 
 
+
+# better variable names, tuple assignment for shorter initialization section 
+class Solution2(object):
+    def isSubsequence(self, s: str, t: str) -> bool:
+        short_str, long_str = s, t 
+        s, l = 0, 0
+
+        if len(short_str) > len(long_str):
+            return False
+
+        if not short_str:
+            return True
+
+        while l < len(long_str):
+            if long_str[l] == short_str[s]:
+                s += 1
+
+            l += 1
+            
+            if s == len(short_str):
+                return True
+
+        return False 
         
