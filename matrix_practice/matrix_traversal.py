64271d03a339ff742dd05ff60f84790fc421db14
robwatsongtr <rwatso@gmail.com>
2024-08-07 11:12:36 -0700

    matrix practice

---
+++ matrix_practice/matrix_traversal.py
@@ -0,0 +1,31 @@
+# matrix traversal
+
+def row_traversal(matrix):
+    for row in matrix:
+        for element in row:
+            print(element)
+    print()
+
+def column_traversal(matrix):
+    for col in range(len(matrix[0])):
+        for row in range(len(matrix)):
+            print(matrix[row][col])
+    print()
+        
+
+
+
+if __name__ == "__main__":
+    matrix = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ]
+    print('row traversal:')
+    row_traversal(matrix)
+
+    print('column traversal')
+    column_traversal(matrix)
+
+
+    
