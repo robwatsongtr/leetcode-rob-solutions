b7e6eedb3f2711b7be8abd67d67fdf322409e5ed
robwatsongtr <rwatso@gmail.com>
2024-08-25 17:00:07 -0700

    weighted graph

--- algo_memorization/weighted_graph.py
+++ algo_memorization/weighted_graph.py
@@ -1,3 +1,5 @@
+import pprint 
+
 class WeightedGraph:
     def __init__(self):
         self.adj_list = {}
@@ -16,3 +18,22 @@ class WeightedGraph:
 
         self.adj_list[node1][node2] = weight
         self.adj_list[node2][node1] = weight 
+
+
+if __name__ == '__main__':
+    g = WeightedGraph()
+
+    g.add_node('San Rafael')
+    g.add_node('Oakland')
+    g.add_node('Milpitas')
+    g.add_node('Santa Cruz')
+    g.add_node('Sunnyvale')
+
+    g.add_edge('San Rafael', 'Oakland', 22)
+    g.add_edge('San Rafael', 'Sunnyvale', 65)
+    g.add_edge('Oakland', 'Milpitas', 48)
+    g.add_edge('Sunnyvale', 'Milpitas', 10)
+    g.add_edge('Santa Cruz', 'Sunnyvale', 36)
+
+    pprint.pprint(g.adj_list)
+    
