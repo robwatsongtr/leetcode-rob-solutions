cf7f214055150afe6e08cf31867488b10daa518e
robwatsongtr <rwatso@gmail.com>
2024-09-06 15:12:53 -0700

    graph_matrix

--- algo_memorization/graph_matrix.py
+++ algo_memorization/graph_matrix.py
@@ -2,5 +2,10 @@ class MatrixGraph:
     def __init__(self, matrix):
         self.matrix = matrix
         self.rows = len(matrix)
-        self.colums = len(matrix[0])
-        self.directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        self.cols = len(matrix[0])
+        self.directions = [(-1, 0), (1, 0), (0, -1), (0, 1)] # up, down, left, right 
+        self.visited = set()
+
+    def is_within_bounds(self, row, col):
+        return 0 <= row < self.rows and 0 <= col < self.cols
+    
