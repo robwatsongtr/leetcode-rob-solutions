296221db8dae392f783144850e78a9b15910a756
robwatsongtr <rwatso@gmail.com>
2024-09-13 14:35:31 -0700

    begin sort class'

--- algo_memorization/sort.py
+++ algo_memorization/sort.py
@@ -1,2 +1,11 @@
 class Sort:
-    pass
+    def swap_helper(self, list, start, end):
+        temp = list[start]
+        list[start] = list[end]
+        list[end] = temp 
+
+    def bubble_sort(self, list):
+        for i in range(len(list) - 1, -1, -1):
+            j = 0
+            
+
