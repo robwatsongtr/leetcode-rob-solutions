7a273cd75ae5bd005453b44a372c3cfcf3344450
robwatsongtr <rwatso@gmail.com>
2024-09-14 14:40:35 -0700

    bubble sort done

--- algo_memorization/sort.py
+++ algo_memorization/sort.py
@@ -5,7 +5,35 @@ class Sort:
         list[end] = temp 
 
     def bubble_sort(self, list):
-        for i in range(len(list) - 1, -1, -1):
-            j = 0
+        list_len = len(list)
+
+        # outer for loop controls the overall process
+        for outer_pass in range(list_len):
+            # used for early exit if no more swaps, that means we're done sorting now 
+            swapped = False
+            # Inner loop compares adjacent elements and swaps them if needed.
+            # The range shrinks with each outer pass because the largest elements "bubble" to the end.
+            for j in range(list_len - outer_pass - 1):
+                if list[j] > list[j + 1]:
+                    self.swap_helper(list, j, j + 1)
+                    swapped = True
+            # no more swaps break early 
+            if not swapped:
+                break
+        
+        return list 
+            
+
+if __name__ == "__main__":
+    arr = [5, 8, 9, 1, 2, 7, 3]
+
+    arr_sorted = Sort().bubble_sort(arr)
+
+    print(f'sorted arr: {arr_sorted}')
+    
+                
+
+
+
             
 
