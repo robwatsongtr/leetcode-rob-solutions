976e24956514cd66db77154ee20630ebe996344a
robwatsongtr <rwatso@gmail.com>
2024-09-14 15:07:20 -0700

    selection sort

--- algo_memorization/sort.py
+++ algo_memorization/sort.py
@@ -1,5 +1,5 @@
 class Sort:
-    def swap_helper(self, list, start, end):
+    def swap(self, list, start, end):
         temp = list[start]
         list[start] = list[end]
         list[end] = temp 
@@ -15,21 +15,42 @@ class Sort:
             # The range shrinks with each outer pass because the largest elements "bubble" to the end.
             for j in range(list_len - outer_pass - 1):
                 if list[j] > list[j + 1]:
-                    self.swap_helper(list, j, j + 1)
+                    self.swap(list, j, j + 1)
                     swapped = True
             # no more swaps break early 
             if not swapped:
                 break
         
         return list 
+    
+    # Selection sort scans the array for the smallest element and when found swaps it to the front 
+    # of the array, swapped to the next unsorted index
+    def selection_sort(self, list):
+        for i in range(len(list)):
+            lowest = i
+            for j in range(i + 1, len(list)):
+                if list[j] < list[lowest]:
+                    lowest = j
+            
+            # The if statement ensures that you only swap when i and lowest refer to different indices.d
+            if i != lowest:
+                self.swap(list, i, lowest)
+        
+        return list 
             
 
+
+
+
 if __name__ == "__main__":
+    do_sort = Sort()
     arr = [5, 8, 9, 1, 2, 7, 3]
 
-    arr_sorted = Sort().bubble_sort(arr)
+    arr_bubble_sorted = do_sort.bubble_sort(arr)
+    arr_selection_sorted = do_sort.selection_sort(arr)
 
-    print(f'sorted arr: {arr_sorted}')
+    print(f'bubble sorted arr: {arr_bubble_sorted}')
+    print(f'selection sorted arr: {arr_selection_sorted}')
     
                 
 
