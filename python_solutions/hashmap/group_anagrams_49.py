9c63dc751fd831883d04a679c28ad08cbea01ce0
robwatsongtr <rwatso@gmail.com>
2024-11-29 17:24:19 -0800

    group anagrams

---
+++ python_solutions/hashmap/group_anagrams_49.py
@@ -0,0 +1,42 @@
+"""
+49. Group Anagrams
+
+Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+Example 1:
+
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+Explanation:
+
+There is no string in strs that can be rearranged to form "bat".
+The strings "nat" and "tan" are anagrams as they can be rearranged to form each other.
+The strings "ate", "eat", and "tea" are anagrams as they can be rearranged to form each other.
+
+Example 2:
+
+Input: strs = [""]
+Output: [[""]]
+
+Example 3:
+
+Input: strs = ["a"]
+Output: [["a"]]
+
+"""
+
+class Solution:
+    def groupAnagrams(self, strs):
+        hashmap = {}
+
+        for s in strs:
+            sorted_str = "".join(sorted(s))
+
+            if sorted_str not in hashmap:
+                hashmap[sorted_str] = []
+
+            # looks up sorted str and append its raw unsorted version from strs array 
+            hashmap[sorted_str].append(s)
+
+        return list(hashmap.values())
