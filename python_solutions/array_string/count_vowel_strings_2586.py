ad1a6ce03ce3956bb0b70150e17564bba9de3410
robwatsongtr <rwatso@gmail.com>
2025-11-10 19:03:41 -0800

    lc 2586 count vowels in string

---
+++ python_solutions/array_string/count_vowel_strings_2586.py
@@ -0,0 +1,46 @@
+"""
+2586. Count the Number of Vowel Strings in Range
+
+You are given a 0-indexed array of string words and two integers left and right.
+
+A string is called a vowel string if it starts with a vowel character and ends with
+ a vowel character where vowel characters are 'a', 'e', 'i', 'o', and 'u'.
+
+Return the number of vowel strings words[i] where i belongs to the inclusive range [left, right].
+
+Example 1:
+
+Input: words = ["are","amy","u"], left = 0, right = 2
+Output: 2
+Explanation: 
+- "are" is a vowel string because it starts with 'a' and ends with 'e'.
+- "amy" is not a vowel string because it does not end with a vowel.
+- "u" is a vowel string because it starts with 'u' and ends with 'u'.
+The number of vowel strings in the mentioned range is 2.
+
+Example 2:
+
+Input: words = ["hey","aeo","mu","ooo","artro"], left = 1, right = 4
+Output: 3
+Explanation: 
+- "aeo" is a vowel string because it starts with 'a' and ends with 'o'.
+- "mu" is not a vowel string because it does not start with a vowel.
+- "ooo" is a vowel string because it starts with 'o' and ends with 'o'.
+- "artro" is a vowel string because it starts with 'a' and ends with 'o'.
+The number of vowel strings in the mentioned range is 3.
+
+"""
+
+class Solution:
+    def vowelStrings(self, words, left: int, right: int) -> int:
+        count = 0
+        vowels = ['a', 'e', 'i', 'o', 'u']
+
+        for i in range(left, right + 1):
+            # also works:
+            # if words[i][0] in vowels and words[i][-1] in vowels: 
+            
+            if words[i].startswith(tuple(vowels)) and words[i].endswith(tuple(vowels)):
+                count += 1
+
+        return count 
