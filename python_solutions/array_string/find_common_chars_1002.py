e329b7723d8ba46c127fc36f0676946bd3e116ba
Rob Watson <rwatso@gmail.com>
2025-12-10 09:49:54 -0800

    comments lc 1002

--- python_solutions/array_string/find_common_chars_1002.py
+++ python_solutions/array_string/find_common_chars_1002.py
@@ -1,4 +1,17 @@
 """
+1002. Find Common Characters
+
+Given a string array words, return an array of all characters that show up in all strings w
+ithin the words (including duplicates). You may return the answer in any order.
+
+Example 1:
+
+Input: words = ["bella","label","roller"]
+Output: ["e","l","l"]
+Example 2:
+
+Input: words = ["cool","lock","cook"]
+Output: ["c","o"]
 
 
 """
